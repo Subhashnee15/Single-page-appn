50a63d39d4f627f29c506f0273a925e7bc1c7eaa
SUBHASHNEE THAKUR <126400709+Subhashnee15@users.noreply.github.com>
2024-05-31 05:25:34 +0530

    Add files via upload
    SAP projects gets completed!!!

--- index.js
+++ index.js
@@ -17,6 +17,38 @@ function changeContent(page) {
         ` ;
         break;
         case 'about' :
+          contentDiv.innerHTML = `
+          <h2>AboutUs </h2>
+          <p> This is the baout page content. Learn more
+          about our purpose or team . </p>
+          <p> We're passionate about creating engaging and informative SPAs.
+          </p>
+          
+          `;
+          break;
+          case 'contact' :
+            contentDiv.innerHTML =
+            `
+            <h2> Contact Us </h2>
+            <p>
+            Feel free to reach us !!
+            </p>
+            <form>
+            <label for = "name">Name: </label>
+            <input type ="text" id="name" name= "name"
+            placeholder = "Your Name" required>
+            <label for = "email">Email </label>
+            <input type ="email" id="email" name= "email"
+            placeholder = "Your email" required>
+            <label for = "message">message: </label>
+            <textarea id="message" name= "message"
+            placeholder = "Your message" rows="4" required>
+            </textarea>
+            <button type = "submit"> Send message </button>
+            </form> ` ;
+            break;
+            default: 
+            contentDiv.innerHTML = `<h2> Page Not found! </h2>`;
             
     }
 }
